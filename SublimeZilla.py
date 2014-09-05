0e08e3b9e9bc5c7324311c6d01fa24dca3915f6f
Jurgens <tooone777@gmail.com>
2014-09-05 14:02:58 +0200

    Add more checks and fixes #2
    Changed indentation from spaces to tabs.
    Added more checks for Host, Username and Port.
    Also added another check for sitemanager.xml in the run() method with a better error message on how to add another location for site manager.

--- SublimeZilla.py
+++ SublimeZilla.py
@@ -5,232 +5,254 @@ import re
 
 from xml.dom import minidom
 
+
 class SublimeZillaCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		# check settings
-		settings = sublime.load_settings("SublimeZilla.sublime-settings")
 
-		if settings.get("filezilla_db_path", "") == "":
-			self.window.show_input_panel( 'Browse to FileZilla directory', self.get_xml(), self.save_config, None, None )
-		else:
-			self.quick_panel()
+    def run(self):
+        # check settings
+        settings = sublime.load_settings("SublimeZilla.sublime-settings")
 
-	def quick_panel(self):
-		server_names = self.get_server_names()
-		self.window.show_quick_panel( server_names, self.server_chosen, sublime.MONOSPACE_FONT )
+        # This is not very elegant but the get_xml() method will throw an error popup in SublimeText
+        if not self.get_xml():
+            return None
 
-	# A server was chosen
-	def server_chosen(self, server_index):
-		if server_index == -1:
-			return
+        if settings.get("filezilla_db_path", "") == "":
+            self.window.show_input_panel('Browse to FileZilla directory', self.get_xml(), self.save_config, None, None)
+        else:
+            self.quick_panel()
 
-		self.server = self.get_server(server_index)
+    def quick_panel(self):
+        server_names = self.get_server_names()
+        self.window.show_quick_panel(server_names, self.server_chosen, sublime.MONOSPACE_FONT)
 
-		# Copy default SFTP config to current project root
-		packages_path = sublime.packages_path()
-		sftp_path = packages_path + "/SFTP/"
-		sftp_path_default_config = sftp_path + "SFTP.default-config"
+    # A server was chosen
+    def server_chosen(self, server_index):
+        if server_index == -1:
+            return
 
-		if not os.path.exists( sftp_path ):
-			sublime.error_message("SFTP not installed. Exiting...")
-			return None
+        self.server = self.get_server(server_index)
 
-		if not os.path.exists( sftp_path_default_config ):
-			sublime.error_message("SFTP default config not found. Exiting...")
-			return None
+        # Copy default SFTP config to current project root
+        packages_path = sublime.packages_path()
+        sftp_path = packages_path + "/SFTP/"
+        sftp_path_default_config = sftp_path + "SFTP.default-config"
 
-		self.set_sftp_config();
+        if not os.path.exists(sftp_path):
+            sublime.error_message("SFTP not installed. Exiting...")
+            return None
 
-	def set_sftp_config(self):
+        if not os.path.exists(sftp_path_default_config):
+            sublime.error_message("SFTP default config not found. Exiting...")
+            return None
 
-		import json
+        self.set_sftp_config()
 
-		server_config = {
-			"host": self.server["host"],
-			"user": self.server["user"],
-			"password": self.server["password"],
-			"port": self.server["port"],
-			"remote_path": self.server["remote_path"]
-		}
+    def set_sftp_config(self):
 
-		# Open a new buffer and name it
-		config_view = self.window.new_file()
-		config_view.set_name("sftp-config.json")
-		config_view.set_syntax_file("Packages/JavaScript/JSON.tmLanguage")
+        import json
 
-		# Check for SFTP config file
-		SFTP_config = sublime.packages_path() + "/SFTP/SFTP.default-config"
-		if os.path.exists( SFTP_config ):
+        server_config = {
+            "host": self.server["host"],
+            "user": self.server["user"],
+            "password": self.server["password"],
+            "port": self.server["port"],
+            "remote_path": self.server["remote_path"]
+        }
 
-			# Grab the snippet text
-			f = open(SFTP_config, 'r')
-			config_json = f.read()
-			f.close()
+        # Open a new buffer and name it
+        config_view = self.window.new_file()
+        config_view.set_name("sftp-config.json")
+        config_view.set_syntax_file("Packages/JavaScript/JSON.tmLanguage")
 
-			# Replace snippet defaults with found variables
-			snippet = self.intercept_sftp( server_config, config_json )
+        # Check for SFTP config file
+        SFTP_config = sublime.packages_path() + "/SFTP/SFTP.default-config"
+        if os.path.exists(SFTP_config):
 
-			# Insert the snippet. It can be tabbed through!
-			config_view.run_command("insert_snippet", {'contents': snippet})
+            # Grab the snippet text
+            f = open(SFTP_config, 'r')
+            config_json = f.read()
+            f.close()
 
-		else:
-			config_json = json.dumps(server_config, sort_keys=False, indent=4, separators=(',', ': '))
-			config_view.run_command("insert_snippet", {'contents': config_json})
+            # Replace snippet defaults with found variables
+            snippet = self.intercept_sftp(server_config, config_json)
 
-	# A function that takes the raw snippet text from /SFTP/SFTP.default-config and replaces it with the FZ db
-	# default_sftp is the object that contains the FileZilla db entries
-	# sftp_snippet is the SFTP snippet contents
-	def intercept_sftp(self, default_sftp, sftp_snippet):
+            # Insert the snippet. It can be tabbed through!
+            config_view.run_command("insert_snippet", {'contents': snippet})
 
-		# print default_sftp["host"]
-		variableTest = ""
+        else:
+            config_json = json.dumps(server_config, sort_keys=False, indent=4, separators=(',', ': '))
+            config_view.run_command("insert_snippet", {'contents': config_json})
 
-		new_snippet = re.sub(r'(\$\{\d{1,2}\:)example.com(\})', 	r'\g<1>' + default_sftp["host"] + r'\g<2>', 		sftp_snippet, re.M )
-		new_snippet = re.sub(r'(\$\{\d{1,2}\:)username(\})', 		r'\g<1>' + default_sftp["user"] + r'\g<2>', 		new_snippet, re.M )
+    # A function that takes the raw snippet text from /SFTP/SFTP.default-config and replaces it with the FZ db
+    # default_sftp is the object that contains the FileZilla db entries
+    # sftp_snippet is the SFTP snippet contents
+    def intercept_sftp(self, default_sftp, sftp_snippet):
 
-		# Remove // before password key
-		new_snippet = re.sub(r'(\$\{\d{1,2}\:)//(\})("password":)', r'\g<3>', 											new_snippet, re.M )
-		new_snippet = re.sub(r'(\$\{\d{1,2}\:)password(\})', 		r'\g<1>' + default_sftp["password"] + r'\g<2>', 	new_snippet, re.M )
+        # print default_sftp["host"]
+        variableTest = ""
 
-		#@TODO: fix the port issue.. for some reason it inserts "Q}".. testing on http://gskinner.com/RegExr/ provides the results I expect.. strange
-		# new_snippet = re.sub(r'(\$\{\d{1,2}\:)22(\})', 				r'\g<1>' + str(default_sftp["port"]) + r'\g<2>', 	new_snippet, re.M )
-		new_snippet = re.sub(r'(\$\{\d{1,2}\:)/example/path/(\})',  r'\g<1>' + default_sftp["remote_path"] + r'\g<2>', 	new_snippet, re.M )
+        new_snippet = re.sub(r'(\$\{\d{1,2}\:)example.com(\})', r'\g<1>' + default_sftp["host"] + r'\g<2>',
+                             sftp_snippet, re.M)
+        new_snippet = re.sub(r'(\$\{\d{1,2}\:)username(\})', r'\g<1>' + default_sftp["user"] + r'\g<2>', new_snippet,
+                             re.M)
 
-		return new_snippet
+        # Remove // before password key
+        new_snippet = re.sub(r'(\$\{\d{1,2}\:)//(\})("password":)', r'\g<3>', new_snippet, re.M)
+        new_snippet = re.sub(r'(\$\{\d{1,2}\:)password(\})', r'\g<1>' + default_sftp["password"] + r'\g<2>',
+                             new_snippet, re.M)
 
-	def get_xml(self):
+        # @TODO: fix the port issue.. for some reason it inserts "Q}".. testing on http://gskinner.com/RegExr/ provides the results I expect.. strange
+        # new_snippet = re.sub(r'(\$\{\d{1,2}\:)22(\})', 				r'\g<1>' + str(default_sftp["port"]) + r'\g<2>', 	new_snippet, re.M )
+        new_snippet = re.sub(r'(\$\{\d{1,2}\:)/example/path/(\})', r'\g<1>' + default_sftp["remote_path"] + r'\g<2>',
+                             new_snippet, re.M)
 
-		user_home = os.curdir
+        return new_snippet
 
-		if 'HOME' in os.environ:
-			user_home = os.environ['HOME']
-		elif os.name == 'posix':
-			user_home = os.path.expanduser("~")
-		elif os.name == 'nt':
-			if 'HOMEPATH' in os.environ and 'HOMEDRIVE' in os.environ:
-				user_home = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']
-		else:
-			user_home = os.environ['HOMEPATH']
+    def get_xml(self):
 
-		# The default locations for FileZilla's XML database
-		if os.name == 'nt':
-			default_xml = user_home + os.sep + "AppData\\Roaming\\FileZilla\\sitemanager.xml"
-		elif os.name == 'posix':
-			default_xml = user_home + os.sep + ".filezilla/sitemanager.xml"
+        user_home = os.curdir
 
-		settings = sublime.load_settings("SublimeZilla.sublime-settings")
-		path = settings.get("filezilla_db_path", default_xml)
+        if 'HOME' in os.environ:
+            user_home = os.environ['HOME']
+        elif os.name == 'posix':
+            user_home = os.path.expanduser("~")
+        elif os.name == 'nt':
+            if 'HOMEPATH' in os.environ and 'HOMEDRIVE' in os.environ:
+                user_home = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']
+        else:
+            user_home = os.environ['HOMEPATH']
 
-		if not os.path.exists( path ):
-			path = settings.get("filezilla_db_path2", default_xml)
+        # The default locations for FileZilla's XML database
+        if os.name == 'nt':
+            default_xml = user_home + os.sep + "AppData\\Roaming\\FileZilla\\sitemanager.xml"
+        elif os.name == 'posix':
+            default_xml = user_home + os.sep + ".filezilla/sitemanager.xml"
 
-			if not os.path.exists( path ):
-				sublime.error_message("File sitemanager.xml not found. Is FileZilla installed?")
-				return None
+        settings = sublime.load_settings("SublimeZilla.sublime-settings")
+        path = settings.get("filezilla_db_path", default_xml)
 
-		return path
+        if not os.path.exists(path):
+            path = settings.get("filezilla_db_path2", default_xml)
+
+            if not os.path.exists(path):
+                msg = "File sitemanager.xml not found. Is FileZilla installed? \n\n"
+                msg += "If the sitemanager.xml file is located elsewhere please see https://github.com/ment4list/SublimeZilla#alternate-locations"
+                sublime.error_message(msg)
+                return None
+
+        return path
 
 
-	def save_config(self, filezilla_db_path):
-		settings = sublime.load_settings("SublimeZilla.sublime-settings")
-		settings.set("filezilla_db_path", filezilla_db_path)
-		sublime.save_settings("SublimeZilla.sublime-settings")
+    def save_config(self, filezilla_db_path):
+        settings = sublime.load_settings("SublimeZilla.sublime-settings")
+        settings.set("filezilla_db_path", filezilla_db_path)
+        sublime.save_settings("SublimeZilla.sublime-settings")
 
-		# Now, show the quick panel
-		self.quick_panel()
+        # Now, show the quick panel
+        self.quick_panel()
 
-	def create_project_config(self):
-		self.config_view = self.window.new_file()
-		self.window.run_command('sublime_zilla_config', new_file=self.config_view)
+    def create_project_config(self):
+        self.config_view = self.window.new_file()
+        self.window.run_command('sublime_zilla_config', new_file=self.config_view)
 
-	def get_server_names(self):
-		self.servers = self.get_server_entries()
-		server_names = []
+    def get_server_names(self):
+        self.servers = self.get_server_entries()
+        server_names = []
 
-		for server_name in self.servers:
-			server_names.append(server_name["name"])
+        for server_name in self.servers:
+            server_names.append(server_name["name"])
 
-		return server_names
+        return server_names
 
-	def get_server_entries(self):
-		xmldoc = minidom.parse( self.get_xml() )
-		itemlist = xmldoc.getElementsByTagName('Server')
-		server_array = []
+    def get_server_entries(self):
+        xmldoc = minidom.parse(self.get_xml())
+        itemlist = xmldoc.getElementsByTagName('Server')
+        server_array = []
 
-		for server in itemlist :
+        for server in itemlist:
 
-			server_obj = {}
+            server_obj = {}
 
-			Name = server.getElementsByTagName('Name')
-			NameVal = Name[0].firstChild.nodeValue
-			server_obj["name"] = str(NameVal)
+            Name = server.getElementsByTagName('Name')
+            if Name[0].firstChild is not None:
+                NameVal = Name[0].firstChild.nodeValue
+                server_obj["name"] = str(NameVal)
+            else:
+                server_obj["name"] = ""
 
-			Host = server.getElementsByTagName('Host')
-			HostVal = Host[0].firstChild.nodeValue
-			server_obj["host"] = str(HostVal)
+            Host = server.getElementsByTagName('Host')
+            if Host[0].firstChild is not None:
+                HostVal = Host[0].firstChild.nodeValue
+                server_obj["host"] = str(HostVal)
+            else:
+                server_obj["host"] = ""
 
-			Port = server.getElementsByTagName('Port')
-			PortVal = Port[0].firstChild.nodeValue
-			server_obj["port"] = str(PortVal)
+            Port = server.getElementsByTagName('Port')
+            if Port[0].firstChild is not None:
+                PortVal = Port[0].firstChild.nodeValue
+                server_obj["port"] = str(PortVal)
+            else:
+                server_obj["port"] = ""
 
-			User = server.getElementsByTagName('User')
-			UserVal = User[0].firstChild.nodeValue
-			server_obj["user"] = str(UserVal)
+            User = server.getElementsByTagName('User')
+            if User[0].firstChild is not None:
+                UserVal = User[0].firstChild.nodeValue
+                server_obj["user"] = str(UserVal)
+            else:
+                server_obj["user"] = ""
 
-			Pass = server.getElementsByTagName('Pass')
-			if( Pass[0].firstChild is not None ):
-			    PassVal = Pass[0].firstChild.nodeValue
-			    server_obj["password"] = str(PassVal)
-			else:
-			    server_obj["password"] = ""
+            Pass = server.getElementsByTagName('Pass')
+            if Pass[0].firstChild is not None:
+                PassVal = Pass[0].firstChild.nodeValue
+                server_obj["password"] = str(PassVal)
+            else:
+                server_obj["password"] = ""
 
-			LocalDir = server.getElementsByTagName('LocalDir')
+            LocalDir = server.getElementsByTagName('LocalDir')
+            if LocalDir[0].firstChild is not None:
+                LocalDirVal = LocalDir[0].firstChild.nodeValue
+                server_obj["local_path"] = str(LocalDirVal)
+            else:
+                server_obj["local_path"] = ""
 
-			if( LocalDir[0].firstChild is not None ):
-				LocalDirVal = LocalDir[0].firstChild.nodeValue
-				server_obj["local_path"] = str(LocalDirVal)
-			else:
-				server_obj["local_path"] = ""
+            RemoteDir = server.getElementsByTagName('RemoteDir')
+            if RemoteDir[0].firstChild is not None:
+                RemoteDirVal = RemoteDir[0].firstChild.nodeValue
+                server_obj["remote_path"] = self.convertRemoteDir(str(RemoteDirVal))
+            else:
+                server_obj["remote_path"] = ""
 
-			RemoteDir = server.getElementsByTagName('RemoteDir')
+            # Add this server to the array
+            server_array.append(server_obj)
 
-			if( RemoteDir[0].firstChild is not None ):
-				RemoteDirVal = RemoteDir[0].firstChild.nodeValue
-				server_obj["remote_path"] = self.convertRemoteDir( str(RemoteDirVal) )
-			else:
-				server_obj["remote_path"] = ""
+        return server_array
 
-			# Add this server to the array
-			server_array.append(server_obj)
+    # Converts FileZilla's weird export method for remote directories to actual directories
+    def convertRemoteDir(self, filezilla_dir):
 
-		return server_array
+        # @Note botg's comment here: http://forum.filezilla-project.org/viewtopic.php?f=1&t=15923 might shed some light on why FileZilla exports the directories this way
 
-	# Converts FileZilla's weird export method for remote directories to actual directories
-	def convertRemoteDir( self, filezilla_dir ):
+        regex = "^((\d{1,2}\s){3})(.+)?$"
+        modifiers = "gm"
 
-		# @Note botg's comment here: http://forum.filezilla-project.org/viewtopic.php?f=1&t=15923 might shed some light on why FileZilla exports the directories this way
+        re_compile = re.compile(regex, re.M)
+        results = re.split(regex, filezilla_dir)
 
-		regex = "^((\d{1,2}\s){3})(.+)?$"
-		modifiers = "gm"
+        try:
+            fz_remote_dir = results[3]
+            # Replace all instances of \s\d{1,2}\s within capture group 3 with a "/" because it is seen as a directory by FileZilla
+            slash_regex = "\s\d{1,2}\s"
 
-		re_compile = re.compile( regex, re.M )
-		results = re.split( regex, filezilla_dir )
+            with_slashes = re.split(slash_regex, fz_remote_dir)
 
-		try:
-			fz_remote_dir = results[3]
-			# Replace all instances of \s\d{1,2}\s within capture group 3 with a "/" because it is seen as a directory by FileZilla
-			slash_regex = "\s\d{1,2}\s"
+            return "/" + "/".join(with_slashes) + "/"
 
-			with_slashes = re.split( slash_regex, fz_remote_dir )
+        except IndexError:
+            return '/'
 
-			return "/" + "/".join( with_slashes ) + "/"
+    def get_server(self, server_index):
+        return self.servers[server_index]
 
-		except IndexError:
-			return '/'
-
-	def get_server(self, server_index):
-		return self.servers[server_index]
-
-	# getDirectories taken from Packages\SideBarEnhancements\sidebar\SideBarProject.py
-	def getDirectories(self):
-		return sublime.active_window().folders()
+    # getDirectories taken from Packages\SideBarEnhancements\sidebar\SideBarProject.py
+    def getDirectories(self):
+        return sublime.active_window().folders()
